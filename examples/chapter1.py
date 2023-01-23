465dd1230b0acb089a74a51178fcda8b01add20d
Anton Shefer <rezajkee@mail.ru>
2023-01-23 23:18:33 +0500

    Add example

--- examples/chapter1.py
+++ examples/chapter1.py
@@ -21,3 +21,29 @@ suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+
+import math
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
